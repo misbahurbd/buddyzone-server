e16665c990893cd5dbd3d3f52261831ab2bc1455
Misbahur Rahman <misbahurbd@gmail.com>
2025-11-23 10:46:18 +0600

    fix prisma version issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './infrastructure/prisma/prisma.module';
 import { RedisModule } from './infrastructure/redis/redis.module';
-import { ConfigModule } from '@nestjs/config';
 import { validateEnv } from './common/validators/env-validator';
 
 @Module({
