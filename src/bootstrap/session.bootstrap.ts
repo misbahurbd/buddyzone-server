9bed1f8b5801d17d1b8b850d9de5e03857e82e67
Misbahur Rahman <misbahurbd@gmail.com>
2025-11-25 07:04:03 +0600

    update cookie config

--- src/bootstrap/session.bootstrap.ts
+++ src/bootstrap/session.bootstrap.ts
@@ -1,4 +1,5 @@
 import passport from 'passport';
+import { v4 as uuid } from 'uuid';
 import { RedisStore } from 'connect-redis';
 import { ConfigService } from '@nestjs/config';
 import { INestApplication } from '@nestjs/common';
@@ -21,23 +22,25 @@ export const configureSession = (
   const sessionRolling = configService.getOrThrow<boolean>('SESSION_ROLLING');
 
   const sessionOptions: SessionOptions = {
+    genid: () => uuid(),
+    store: new RedisStore({
+      client: redisClient,
+      prefix: 'auth:',
+      ttl: sessionTTL,
+    }),
     name: sessionName,
     secret: sessionSecret,
     resave: false,
     saveUninitialized: false,
-    store: new RedisStore({
-      client: redisClient,
-      prefix: 'session:',
-      ttl: sessionTTL,
-    }),
+    rolling: sessionRolling,
+    proxy: isProduction,
     cookie: {
       path: '/',
       httpOnly: true,
       secure: isProduction,
       maxAge: sessionTTL * 1000,
-      sameSite: 'lax',
+      sameSite: isProduction ? 'strict' : 'lax',
     },
-    rolling: sessionRolling,
   };
 
   app.use(session(sessionOptions));
